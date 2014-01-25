c3cafa079cffd72a7903f386e66c9da5c3d691e0
Patrick Haas <p@trickhaas.de>
2014-01-25 15:12:13 +0100

    Third commit

--- procrastibot.py
+++ procrastibot.py
@@ -1,5 +1,7 @@
+import requests
 import os
-from flask import Flask
+import json
+from flask import Flask, request
 
 access_token = "6f657c8067e701314824028b45ba2ff5"
 bot_id = "999711d482b0075f6dea2b35e7"
@@ -8,15 +10,19 @@ group_id = "6870628"
 bot_url = "https://api.groupme.com/v3/bots/post"
 
 app = Flask(__name__)
+app.debug = True
 
 @app.route('/')
 def hello():
-	payload = { 'bot_id': configuration.bot_id,'text': "http://reddit.com/r/pics" }
-	r = requests.post(configuration.bot_url, params=configuration.params, data=json.dumps(payload))
-	return 'SPAM'
+	#payload = { 'bot_id': bot_id,'text': "http://reddit.com/r/pics" }
+	#r = requests.post(bot_url, params=params, data=json.dumps(payload))
+	return 'OK'
 
-#@app.route('/ping')
-#def ping():
-#	payload = { 'bot_id': configuration.bot_id,'text': "http://reddit.com/r/pics" }
-#	r = requests.post(configuration.bot_url, params=configuration.params, data=json.dumps(payload))
-#	return 'OK'
+@app.route('/ping')
+def ping():
+	message = request.form
+	print(message)
+
+	#payload = { 'bot_id': bot_id,'text': "http://reddit.com/r/pics" }
+	#r = requests.post(bot_url, params=params, data=json.dumps(payload))
+	return 'OK'
