de2a5d7175839231153c67d8d2f6215fe86484f8
Patrick Haas <p@trickhaas.de>
2014-01-28 09:30:42 +0100

    kjkjl

--- procrastibot.py
+++ procrastibot.py
@@ -7,30 +7,38 @@ import re
 from flask import Flask, request
 import random
 
-access_token = "6f657c8067e701314824028b45ba2ff5"
-bot_id = "999711d482b0075f6dea2b35e7"
-params = { "token": access_token }
-group_id = "6870628"
-bot_url = "https://api.groupme.com/v3/bots/post"
+import config as c
 
 app = Flask(__name__)
 app.debug = True
 
+running_local=False
+
+def send_message(text):
+	#group_id = "6870628"
+	params = {'token': c.config['access_token']}
+	
+	if (not running_local):
+		payload = {'bot_id': c.config['bot_id'], 'text': text}
+		r = requests.post(c.config['bot_url'], params=params, data=json.dumps(payload))
+		app.logger.debug("send -> {}".format(text))
+	else:
+		app.logger.debug("send -> {}".format(text))
+		
+
 def get_random_reddit_link():
 	r = requests.get("http://www.reddit.com/r/funny/hot.json")
 	data = r.json()
+
 	i = random.randint(0, len(data['data']['children'])-1)
 	#return (data['data']['children'][i]['data']['title'] + " -- http://reddit.com" + data['data']['children'][i]['data']['permalink'])
 	#return (data['data']['children'][i]['data']['title'] + " -- " + data['data']['children'][i]['data']['url'])
 	title = data['data']['children'][i]['data']['title']
 	url = data['data']['children'][i]['data']['url']
-
 	return (title, url)
 
 @app.route('/')
 def hello():
-	#payload = { 'bot_id': bot_id,'text': "http://reddit.com/r/pics" }
-	#r = requests.post(bot_url, params=params, data=json.dumps(payload))
 	return 'OK'
 
 @app.route('/ping', methods=['GET', 'POST'])
@@ -41,16 +49,10 @@ def ping():
 
 	if (re.search('prokrast', text, re.I)):
 		(title, url) = get_random_reddit_link()
-		payload = { 'bot_id': bot_id,'text': '{}'.format(url) }
-		r = requests.post(bot_url, params=params, data=json.dumps(payload))
-		payload = { 'bot_id': bot_id,'text': '{}'.format(title) }
-		r = requests.post(bot_url, params=params, data=json.dumps(payload))
-		#payload = { 'bot_id': bot_id,'text': 'komm, du willst es doch auch!!' }
-		#r = requests.post(bot_url, params=params, data=json.dumps(payload))
-		return '{} -- {}'.format(url, title) 
+		send_message(url)
+		send_message(title)
+		
 	elif (re.search('kann[\w ]*nicht', text, re.I)):
-		payload = { 'bot_id': bot_id,'text': 'kann-nicht wohnt in der will-nicht-straße, {}'.format(name) }
-		#payload = { 'bot_id': bot_id,'text': 'kann-nicht wohnt in der will-nicht-straße, {}'.format(name) }
-		r = requests.post(bot_url, params=params, data=json.dumps(payload))
-
+		send_message("kann-nicht wohnt in der will-nicht-straße, {}".format(name))
+		
 	return 'OK'
