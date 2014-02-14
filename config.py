10d0e24545f09faaeb146ecb42efbecaa2f8ddca
Patrick Haas <p@trickhaas.de>
2014-02-14 20:34:44 +0100

    repair config

---
+++ config.py
@@ -0,0 +1,29 @@
+Configuration = {
+	'prod_config': {
+		'access_token': "6f657c8067e701314824028b45ba2ff5",
+		'bot_id': "999711d482b0075f6dea2b35e7",
+		'bot_url': "https://api.groupme.com/v3/bots/post",
+		'dropbox_key': '1dqiqqxhbb69i6a',
+		'dropbox_secret': '64cs9gbziabwfxo',
+		'dropbox_folder': '/prokrastibot',
+		'imgflip_get_memes': 'https://api.imgflip.com/get_memes',
+		'imgflip_caption_image': 'https://api.imgflip.com/caption_image',
+		'imgflip_username': 'p7haas',
+		'imgflip_password': '5215846'
+	},
+
+	'test_config': {
+		'access_token': "6f657c8067e701314824028b45ba2ff5",
+		'bot_id': "36b67b11dccf4b33f3755db06c",
+		'bot_url': "https://api.groupme.com/v3/bots/post",
+		'dropbox_key': '1dqiqqxhbb69i6a',
+		'dropbox_secret': '64cs9gbziabwfxo',
+		'dropbox_folder': '/prokrastibot',
+		'imgflip_get_memes': 'https://api.imgflip.com/get_memes',
+		'imgflip_caption_image': 'https://api.imgflip.com/caption_image',
+		'imgflip_username': 'p7haas',
+		'imgflip_password': '5215846'
+	}
+}
+
+config = Configuration['prod_config']
