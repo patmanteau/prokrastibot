7f48e1d5404f32c6f8ecd42a2dbf2bdc80d2e281
Patrick Haas <p@trickhaas.de>
2015-02-16 19:25:58 +0100

    - Code cleanups

--- brain.py
+++ brain.py
@@ -10,35 +10,6 @@ import groupme
 import logging
 logger = logging.getLogger(__name__)
 
-class RegexResponder:
-    def __init__(self, regex, regex_options, func):
-        """ Construct a Responder that:
-            1. matches a string against regex using regex_options
-            2. passes matched groups to func
-            3. returns func's result
-        """
-        self.regex = regex
-        self.regex_options = regex_options
-        self.func = func
-
-    def matches(self, message):
-        """ Check if message text matches self.regex """
-        return bool(re.search(self.regex, message.text, self.regex_options))
-
-    def respond_to(self, message):
-        match_obj = re.search(self.regex, message.text, self.regex_options)
-        if match_obj:
-            return self.func(message, match_obj.groups())
-
-def get_memes():
-    memes = {}
-    # get memes from imgflip
-    memereq = requests.get(c.config['imgflip_get_memes']).json()
-    if memereq['success']==True:
-        memes = memereq['data']['memes']
-    
-    return memes
-
 
 def _prokrast(message, groups):
     logger.debug("getting link")
@@ -63,61 +34,54 @@ def chunks(l, n):
     for i in range(0, len(l), n):
         yield l[i:i+n]
 
+
 def _meme(message, groups):
-    logger.debug("meme wanted: {} -- {} -- {}".format(message.sender, message.text, groups))
-    
-    meme_id = groups[0].strip().lower().replace(' ', '_') if len(groups) > 0 else None
-    line1 = groups[1] if len(groups) > 1 else None
-    line2 = groups[2] if len(groups) > 2 else None
-    
-    logger.debug("doing meme: {} -- {} -- {}".format(meme_id, line1, line2))
-    
-    if meme_id:
-        #memes = get_memes()
-        if meme_id in c.known_memes:
-            params = {
-                'template_id': c.known_memes[meme_id],
-                'username': c.config['imgflip_username'],
-                'password': c.config['imgflip_password']
-            }
-            if line1:
-                params['text0'] = line1
-            if line2:
-                params['text1'] = line2
+    """ 
+    """
+    logger.debug('_meme: {}, {}'.format(message, groups))
 
-            logger.debug("requesting meme with params: {}".format(str(params)))
+    meme_id = groups[0].strip().lower().replace(' ', '_') if len(groups) > 0 else None    
+    if meme_id in c.known_memes:
+        params = {
+            'template_id': c.known_memes[meme_id],
+            'username': c.config['imgflip_username'],
+            'password': c.config['imgflip_password'],
+            'text0': groups[1] if len(groups)>1 else '',
+            'text1': groups[2] if len(groups)>2 else '',
+        }
 
-            req = requests.post(c.config['imgflip_caption_image'], params=params).json()
-
-            logger.debug("resulting request: {}".format(req))
-            if req['success']:
-                return [req['data']['url']]
+        logger.debug("_meme: requesting imgflip meme (params: {})".format(str(params)))
+        req = requests.post(c.config['imgflip_caption_image'], params=params).json()
+        if req['success']:
+            return [req['data']['url']]
     return []
+
 
 def _y_u_no(message, groups):
     meme_id = c.known_memes['y_u_no']
-    line1 = groups[0] if len(groups) > 0 else None
-    line2 = groups[1] if len(groups) > 1 else None
-
     params = {
         'template_id': meme_id,
         'username': c.config['imgflip_username'],
-        'password': c.config['imgflip_password']
+        'password': c.config['imgflip_password'],
+        'text0': groups[0] if len(groups)>0 else '',
+        'text1': groups[1] if len(groups)>1 else '',
     }
-    if line1:
-        params['text0'] = line1
-    if line2:
-        params['text1'] = line2
 
     req = requests.post(c.config['imgflip_caption_image'], params=params).json()
+
     if req['success']:
         return [req['data']['url']]
-    return []
+    else:
+        return []
 
 
-__registered = []
-def register(regex, re_opts, handler):
-    __registered.append(RegexResponder(regex, re_opts, handler))
+__registered = {}
+def register(regex, regex_opts, func):
+    __registered[regex] = {
+        'regex_opts': regex_opts,
+        'func': func,
+    }
+
 
 register('^!meme (.+)\|(.+)\|(.*)', re.I, _meme)
 register('^(y u no) (.+)', re.I, _y_u_no)
@@ -143,9 +107,13 @@ register(
     lambda message, groups:
         ["kann-nicht wohnt in der will-nicht-straße, {}".format(message.sender)]
 )
-            
+    
+
 def answer(message):
-    for r in __registered:
-        if r.matches(message):
-            return r.respond_to(message)
+    for regex, handler in __registered.items():
+        match_obj = re.search(regex, message.text, handler['regex_opts'])
+        if match_obj:
+            a = handler['func'](message, match_obj.groups())
+            if a:
+                return a
     return []
